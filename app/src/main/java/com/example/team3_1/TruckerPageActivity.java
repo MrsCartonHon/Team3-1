a284987fdad7adbca8c4338863f201373df6e67c
sam-mcgrath123 <sammcgrath2003@gmail.com>
2021-03-22 14:46:44 -0500

    Simple commit to support Ty's okta version with the page that I'm designing.

---
+++ app/src/main/java/com/example/team3_1/TruckerPageActivity.java
@@ -0,0 +1,30 @@
+package com.example.team3_1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class TruckerPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trucker_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+/*        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+}
