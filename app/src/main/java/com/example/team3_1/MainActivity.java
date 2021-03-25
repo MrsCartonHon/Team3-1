ec8abbef0a058d4c5243491e3de930031d65bd51
Ty <tygross025@gmail.com>
2021-03-25 14:55:37 -0500

    removed MainActivity Sign in Page and redirected to Log in page instead

--- app/src/main/java/com/example/team3_1/MainActivity.java
+++ app/src/main/java/com/example/team3_1/MainActivity.java
@@ -3,18 +3,13 @@ package com.example.team3_1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 
-import com.okta.oidc.AuthenticationPayload;
-import com.okta.oidc.AuthorizationStatus;
 import com.okta.oidc.Okta;
 import com.okta.oidc.RequestCallback;
-import com.okta.oidc.ResultCallback;
 import com.okta.oidc.net.response.UserInfo;
 import com.okta.oidc.util.AuthorizationException;
 
@@ -27,22 +22,13 @@ public class MainActivity extends AppCompatActivity {
         if (oktaManager == null){
             oktaManager = new OktaManager(this);
         }
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         if(oktaManager.isAuthenticated()){
             getUserProfile();
         } else {
             navigateToLogin();
         }
-        setupOktaCallback();
-
-        Button signInBtn = findViewById(R.id.signInBtn);
-        Activity thisclass = this;
-        signInBtn.setOnClickListener(v -> {
-            AuthenticationPayload payload = new AuthenticationPayload.Builder()
-                    .build();
-            MainActivity.oktaManager.signIn(thisclass, payload);
-        });
     }
 
 
@@ -62,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(String error, AuthorizationException exception) {
-                Log.d("MainActivity", exception + "");
+                Log.d("LoginActivity", "error");
+                navigateToLogin();
             }
         });
     }
@@ -76,33 +63,5 @@ public class MainActivity extends AppCompatActivity {
         }
         startActivity(intent);
         finish();
-    }
-
-    private void setupOktaCallback(){
-        MainActivity.oktaManager.registerWebAuthCallback(getAuthCallback(), this);
-    }
-
-    private ResultCallback getAuthCallback(){
-        ResultCallback object = new ResultCallback<AuthorizationStatus, AuthorizationException>() {
-            @Override
-            public void onSuccess(@NonNull AuthorizationStatus status){
-                if (status == AuthorizationStatus.AUTHORIZED){
-                    Log.d("LoginActivity", "AUTHORIZED");
-                    getUserProfile();
-                } else if (status == AuthorizationStatus.SIGNED_OUT){
-                    //clear session
-                    Log.d("LoginActivity", "SIGNED OUT");
-                }
-            }
-            @Override
-            public void onCancel(){
-                Log.d("LoginActivity", "Canceled");
-            }
-            @Override
-            public void onError(@NonNull String msg, AuthorizationException error){
-                Log.d("LoginActivity", "Error: " + error.errorDescription);
-            }
-        };
-        return  object;
     }
 }
