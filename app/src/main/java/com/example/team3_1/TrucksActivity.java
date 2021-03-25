6fbbc0a9418042ed50cc5c1c58fe86096fb82797
sam-mcgrath123 <sammcgrath2003@gmail.com>
2021-03-25 14:57:26 -0500

    Commit final design of the home page for the truck view. Implement this for demonstration and we should be good for Friday 3/26/21.

---
+++ app/src/main/java/com/example/team3_1/TrucksActivity.java
@@ -0,0 +1,25 @@
+package com.example.team3_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+
+import java.util.LinkedList;
+
+public class TrucksActivity extends AppCompatActivity {
+
+//    private final LinkedList<String> mWordList = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trucks);
+//        for(int i = 0; i<20; i++) {
+//            mWordList.addLast("Word" + i);
+//        }
+    }
+
+
+}
