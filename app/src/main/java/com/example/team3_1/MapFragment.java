16021b63d4d81a549f659c9ec6115f6687848ff4
Ty <tygross025@gmail.com>
2021-04-08 15:18:58 -0500

    added Fragments to to show info

--- app/src/main/java/com/example/team3_1/MapFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.team3_1;
-
-import androidx.fragment.app.Fragment;
-
-public class MapFragment extends Fragment {
-    public MapFragment() {
-        super(R.layout.map_main_content);
-    }
-}
--- app/src/main/java/com/example/team3_1/TrucksActivity.java
+++ app/src/main/java/com/example/team3_1/TrucksActivity.java
@@ -2,36 +2,21 @@ package com.example.team3_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 
+import com.example.team3_1.ui.MapFragment;
+import com.example.team3_1.ui.TruckFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.okta.oidc.AuthenticationPayload;
-import com.okta.oidc.AuthorizationStatus;
-import com.okta.oidc.RequestCallback;
-import com.okta.oidc.ResultCallback;
-import com.okta.oidc.util.AuthorizationException;
-
-import com.leinardi.android.speeddial.SpeedDialActionItem;
-
-import java.util.LinkedList;
 
 public class TrucksActivity extends AppCompatActivity {
 
@@ -40,7 +25,15 @@ public class TrucksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trucks_toolbar);
+        setContentView(R.layout.farm_manager_home); //trucks_toolbar
+        FragmentManager  fragmentManager = getSupportFragmentManager();
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment, TruckFragment.class, null)
+                    .commit();
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Trucks");
         setSupportActionBar(toolbar);
@@ -54,6 +47,7 @@ public class TrucksActivity extends AppCompatActivity {
                          case R.id.trucks:
                              return true;
                          case R.id.map:
+
                              return true;
                      }
                      return true;
