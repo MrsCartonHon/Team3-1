e1022305d283f042f4f5178d0584fa247436a042
sam-mcgrath123 <sammcgrath2003@gmail.com>
2021-04-11 17:10:06 -0500

    Trucks Are Duplicating, More options button has a drop down, many things are hard coded.

--- app/src/main/java/com/example/team3_1/TruckListAdapter.java
+++ app/src/main/java/com/example/team3_1/TruckListAdapter.java
@@ -1,12 +1,75 @@
 package com.example.team3_1;
 
-import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TruckListAdapter extends RecyclerView {
-    public TruckListAdapter(@NonNull Context context) {
-        super(context);
+import java.util.ArrayList;
+
+public class TruckListAdapter extends RecyclerView.Adapter<TruckListAdapter.TruckViewHolder> {
+    private ArrayList<TruckItem> mTruckList;
+
+    public static class TruckViewHolder extends RecyclerView.ViewHolder {
+        public TextView mName;
+        public ImageView mMoreOptions;
+        public TextView mTask;
+        public TextView mETA;
+        public ImageView mCurrentTaskIcon;
+        public ImageView mLocationIcon;
+        public Button mMapButton;
+        public Button mContactButton;
+        public Button mNewTaskButton;
+
+        public TruckViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mName = itemView.findViewById(R.id.name);
+            mMoreOptions = itemView.findViewById(R.id.more_options);
+            mTask = itemView.findViewById(R.id.task);
+            mETA = itemView.findViewById(R.id.eta);
+            mCurrentTaskIcon = itemView.findViewById(R.id.current_task_icon);
+            mLocationIcon = itemView.findViewById(R.id.location_icon);
+            mMapButton = itemView.findViewById(R.id.map_button);
+            mContactButton = itemView.findViewById(R.id.contact_button);
+            mNewTaskButton = itemView.findViewById(R.id.new_task_button);
+        }
+    }
+
+    public TruckListAdapter(ArrayList<TruckItem> truckList) {
+        mTruckList = truckList;
+    }
+
+    @NonNull
+    @Override
+    public TruckViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.truck_item, parent, false);
+        TruckViewHolder evh = new TruckViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TruckViewHolder holder, int position) {
+        TruckItem currentItem = mTruckList.get(position);
+
+        holder.mName.setText(currentItem.getName());
+        holder.mMoreOptions.setImageResource(currentItem.getMoreOptions());
+        holder.mTask.setText(currentItem.getTask());
+        holder.mETA.setText(currentItem.getETA());
+        holder.mCurrentTaskIcon.setImageResource(currentItem.getCurrentTaskIcon());
+        holder.mLocationIcon.setImageResource(currentItem.getLocationIcon());
+        holder.mMapButton.setText(currentItem.getMapButton());
+        holder.mContactButton.setText(currentItem.getContactButton());
+        holder.mNewTaskButton.setText(currentItem.getNewTaskButton());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTruckList.size();
     }
 }
--- app/src/main/java/com/example/team3_1/TrucksActivity.java
+++ app/src/main/java/com/example/team3_1/TrucksActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.okta.oidc.AuthenticationPayload;
 import com.okta.oidc.AuthorizationStatus;
 import com.okta.oidc.RequestCallback;
@@ -24,16 +36,39 @@ import com.okta.oidc.util.AuthorizationException;
 
 import com.leinardi.android.speeddial.SpeedDialActionItem;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class TrucksActivity extends AppCompatActivity {
+    private ArrayList<TruckItem> mTruckList;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Menu fabMenu;
 
-//    private final LinkedList<String> mWordList = new LinkedList<>();
+    private FloatingActionButton buttonInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trucks);
+
+        createTruckList();
+        buildRecyclerView();
+
+        buttonInsert = (FloatingActionButton) findViewById(R.id.fab);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup(v);
+            }
+        });
+
         Button signOutBtn = findViewById(R.id.signOutBtn);
         Activity thisclass = this;
         signOutBtn.setOnClickListener(v -> {
@@ -41,9 +76,25 @@ public class TrucksActivity extends AppCompatActivity {
             MainActivity.oktaManager.logOut(this, signOutCallback(thisclass));
 
         });
-//        for(int i = 0; i<20; i++) {
-//            mWordList.addLast("Word" + i);
-//        }
+    }
+
+    public void insertTruck(int position) {
+        mTruckList.add(position, new TruckItem("Truck " + (position+1), R.drawable.more_options_icon, "Going to Grain Cart", "3:45", R.drawable.current_task_icon, R.drawable.location_icon, "Map", "Contact", "New Task"));
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void createTruckList(){
+        mTruckList = new ArrayList<>();
+        mTruckList.add(new TruckItem("Truck 1", R.drawable.more_options_icon, "Going to grain cart", "3:45", R.drawable.current_task_icon, R.drawable.location_icon, "Map", "Contact", "New Task"));
+    }
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new TruckListAdapter(mTruckList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private RequestCallback<Integer, AuthorizationException> signOutCallback(Activity activity) {
@@ -62,5 +113,36 @@ public class TrucksActivity extends AppCompatActivity {
 
         };
         return object;
+    }
+
+    public void showPopup(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.actions, popup.getMenu());
+        popup.show();
+    }
+    public void showMoreOptions(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.edit_truck, popup.getMenu());
+        popup.show();
+    }
+
+    public void moreOptionsTruck(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.rename_truck:
+                Log.d("RENAME", "RENAME");
+            case R.id.delete_truck:
+                Log.d("DELETE", "DELETE");
+        }
+    }
+
+    public void AddTruck(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_new_truck:
+                int position = mRecyclerView.getAdapter().getItemCount();
+                insertTruck(position);
+
+        }
     }
 }
